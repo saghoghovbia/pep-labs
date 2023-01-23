7ee6036a6414b2cc624560def8e002bd44575ec1
saghoghovbia <lilsamoaj@yahoo.com>
2023-01-23 16:52:09 +0000

    Saved progress on PathParameters 2023-01-23 16:52:09 +0000

--- PathParameters/src/main/java/com/revature/JavalinSingleton.java
+++ PathParameters/src/main/java/com/revature/JavalinSingleton.java
@@ -17,7 +17,8 @@ public class JavalinSingleton {
         app.get("/firstname/{first}", ctx -> {
             
             //write code here
-
+            String name = ctx.pathParam("first");
+            ctx.result(name);
         });
 
 
