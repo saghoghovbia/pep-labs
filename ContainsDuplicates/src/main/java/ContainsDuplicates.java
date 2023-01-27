e6d0b96ae6d501073ad524aaec09acf8c1f357b4
saghoghovbia <lilsamoaj@yahoo.com>
2023-01-27 18:11:45 +0000

    Saved progress on ContainsDuplicates 2023-01-27 18:11:45 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        Map<Integer,Integer> map = new HashMap();
+        for(int i=0; i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }
+            else{
+                return true;
+            }
+        }
+
         return false;
     }
 }
